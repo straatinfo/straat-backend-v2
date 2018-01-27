578b4455beee27d94d5e264ea45093671c91133b
johnhigginsavila <johnhiggins.avila@gmail.com>
2018-01-27 18:36:51 +0800

    [#INCIDENTTYPE]FEAT(Crud): added CRUD ops for incident type A.K.A Category

--- src/route/index.js
+++ src/route/index.js
@@ -1,9 +1,13 @@
 const passport = require('passport');
 const AuthRoute = require('./authentication.route');
+const IncidentRoute = require('./incident.route');
+const IncidentTypeRoute = require('./incidentType.route');
 
 module.exports = (app) => {
   app.get('/', welcomePage);
   app.use('/v1/api/auth', AuthRoute);
+  app.use('/v1/api/incident', IncidentRoute);
+  app.use('/v1/api/incidentType', IncidentTypeRoute);
 };
 
 const welcomePage = (req, res, next) => {
