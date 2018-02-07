9dbbdb2c243591d90fa07426f3a7477778bcb754
johnhigginsavila <johnhiggins.avila@gmail.com>
2018-02-08 01:53:11 +0800

    email

--- helpers/mailing.helper.js
+++ helpers/mailing.helper.js
@@ -2,7 +2,7 @@ const SendGridService = require('../service/sendgrid.service');
 
 const sendRegistrationRequestNotif = (userDetail) => {
   const { fname, lname, email, message } = userDetail;
-  const receiver = 'johnhiggins.avila@gmail.com'; //'seqretary@gmail.com';
+  const receiver = 'seqretary@gmail.com';
   const subject = 'Request for access';
   const mailBody = `<p>Greetings!</p><br/><p>A new user is requesting for an access code.<p/><p>User Details:<p/><p>Email: ${email}<p/><p>Firstname: ${fname}<p/><p>Lastname: ${lname} </p><br /><p>Sincerely,<p>Straat.info-team</p>`;
   return new Promise(async(resolve, reject) => {
