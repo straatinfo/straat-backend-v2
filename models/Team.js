4ffab33c096af5b7e68caedecca9de1eaeedf21c
johnhigginsavila <johnhiggins.avila@gmail.com>
2018-02-09 06:52:41 +0800

    [FEAT]REGISTRATION V2

--- models/Team.js
+++ models/Team.js
@@ -2,12 +2,13 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const teamSchema = new Schema({
-  name: { type: String, required: true, unique: true, index: true },
-  email: { type: String, required: true, unique: true },
+  teamName: { type: String, required: true, unique: true, index: true },
+  teamEmail: { type: String, required: true, unique: true },
   logoUrl: { type: String },
   logoSecuredUrl: { type: String },
   description: { type: String },
-  isVolunteer: { type: Boolean },
+  isVolunteer: { type: Boolean, default: true },
+  isApproved: { type: Boolean, default: false },
   _host: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   teamLeaders: [{
     type: mongoose.Schema.Types.ObjectId, ref: 'TeamLeader'
