45adae4dfcfa8fba88db3efaca16e1a190408f83
johnhigginsavila <johnhiggins.avila@gmail.com>
2018-02-13 03:46:46 +0800

    [#CATEGORIES]FEAT: added get main categories by reportTypeID

--- assets/mail-templates/simple-mails.js
+++ assets/mail-templates/simple-mails.js
@@ -5,33 +5,42 @@ module.exports = {
   sendTeamRequestNotifMail: (teamEmail, teamName) => {
     return `<p>Greetings!</p><br/><p>A new Non-Volunteer Team is requesting for approval.<p/><p>User Details:<p/><p>Email: ${teamEmail}<p/><p>Team Name: ${teamName}<p/><p>Sincerely,<p>newteamrequest@straat.info</p>`;
   },
-  sendReportAForHost: (username, teamName, text, category1, category2) => {
+  sendReportAToHost: (username, teamName,teamEmail, text, category1, category2, location, reportDeepLink) => {
     return `
     <p>NB deze mail niet replyen aub. De mailgegevens van de melder treft u hieronder aan</p>
     <p>Geachte mevrouw, mijnheer,</p>
     <p>${username} van ${teamName} heeft zojuist een melding openbare ruimte gedaan. Zou u deze melding in behandeling willen nemen?</p>
-    <p>	Meldingscategorie 1: ${category1}</p>
+    <p>Meldingscategorie 1: ${category1}</p>
     <p>Meldingscategorie 2: ${(category2) ? category2 : '-'}</p>
-    `
+    <p>Eventuele toelichting: ${(text) ? text : '-'}</p>
+    <p>Locatie: ${location}</p>
+    <p>Zou u een ontvangstbevestiging, het evt inboeknummer en de stand van zaken willen doorgeven aan ${teamEmail}</p>
+    <p>Voor een kaart en eventuele foto’s <a href="${reportDeepLink}">klik hier</a></p>
+    <p>Via deze link kunt u - indien u dat wenst - ook de status van de melding aan het team doorgeven. De wijziging van de status wordt voor iedereen zichtbaar op de kaart van de app straat.info, zodat niet alleen de melder maar ook alle andere gebruikers kunnen zien wat de status is.</p>
+    <p>Veel dank alvast voor uw reactie!</p>
+    <p>Met vriendelijke groet</p>
+    <p>${teamName}</p>
+    </br>
+    <p>PS Deze melding is verzonden met de app straat.info dé app voor buurtpreventie. Als u vragen heeft, of tips ter verbetering over de app, kunt u terecht via het formulier op www.straat.info of via info@straat.info</p>
+    `;
+  },
+  sendReportANotifToTeam: (gender, createdAt, categoryName) => {
+    let greeting;
+    if (gender && gender.toUpperCase() === 'MALE') {
+      greeting = 'Sir,';
+    } else if (gender && gender.toUpperCase() === 'FEMALE') {
+      greeting = 'Madam,';
+    } else {
+      greeting = 'Sir, Madam,';
+    }
+    return `
+    <p>Dear ${greeting}</p>
+    <p>You just made a report ${createdAt}</p>
+    <p>report ${categoryName}</p>
+    <p>This report has been send to the relevant host.</p>
+    <p>Thank you for using straat.info</p>
+    </br>
+    <p>If you have any questions or ideas, could you please use the feedback form in the app?</p>
+    `;
   }
 }
-/*
-"NB deze mail niet replyen aub. De mailgegevens van de melder treft u hieronder aan.
- 
-Geachte mevrouw, mijnheer,
-[chatname of reporter]  van  [name team] heeft zojuist een melding openbare ruimte gedaan. Zou u deze melding in behandeling willen nemen?
- 	Meldingscategorie 1: [category 1]
-	Meldingscategorie 2: [category 2] [if there is no category 2, then field show “-“ sign]
-	Eventuele toelichting: [free text field] [if there is no text entered by reporter, then field show “-“ sign]
-	Locatie: [location: street, postal code, city, Country]
-Zou u een ontvangstbevestiging, het evt inboeknummer en de stand van zaken willen doorgeven aan [emailaddress team]
- 
-Voor een kaart en eventuele foto’s klik hier [the words “klik hier” are hyperlinked with the unique page of the report]
-Via deze link kunt u - indien u dat wenst - ook de status van de melding aan het team doorgeven. De wijziging van de status wordt voor iedereen zichtbaar op de kaart van de app straat.info, zodat niet alleen de melder maar ook alle andere gebruikers kunnen zien wat de status is.
-Veel dank alvast voor uw reactie!
-Met vriendelijke groet
-[team name]
-  
-PS Deze melding is verzonden met de app straat.info dé app voor buurtpreventie. Als u vragen heeft, of tips ter verbetering over de app, kunt u terecht via het formulier op www.straat.info of via info@straat.info
-"
-*/
