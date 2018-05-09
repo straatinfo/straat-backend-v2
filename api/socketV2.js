a87563ad9ed1887d99eadbacc41b591a464aa0d4
Jay Lord Torres <jaylord@globalbrainforce.com>
2018-05-09 11:31:45 +0800

    socket: register on reconnection, rework socket/index

---
+++ api/socketV2.js
@@ -0,0 +1,60 @@
+const ConversationHelper = require('../helpers/conversationV2.helper')
+const MessageHelper = require('../helpers/messageV2.helper')
+const SocketHelper = require('../helpers/socket.helper')
+
+const onSendMessage = async (io, socket, userData, args = {}) => {
+  console.log('onSendMessage: ', args)
+  try {
+    const { user, text, _conversation } = args
+    if (!_conversation) {
+      console.log('Error: Invalid conversation ID')
+      return io.to(socket.id).emit('send-message-v2', {
+        status: 0,
+        message: 'Failed to send Message'
+      })
+    }
+    const conversation = await ConversationHelper.__getConversationById(_conversation)
+    const newMessage = await MessageHelper.__createMessage(_conversation, user._id, text)
+    const payload = {
+      _id: newMessage._id,
+      createdAt: newMessage.createdAt,
+      user: user,
+      text: text
+    }
+    console.log('payload', payload)
+    conversation.participants.map(async function (p) {
+      const findSocket = await SocketHelper.findSocketByUser(p._user)
+      console.log('findSocket', p)
+      if (findSocket.socket) {
+        io.to(findSocket.socket._socket).emit('new-message', {
+          status: 1,
+          message: 'Updated message',
+          _conversation: _conversation,
+          payload: payload,
+          conversation: {
+            _id: conversation._id,
+            title: conversation.title,
+            type: conversation.type,
+            _profilePic: conversation._profilePic
+          }
+        })
+      }
+    })
+    console.log('Success: Successfully updated listeners')
+    io.to(socket.id).emit('send-message-v2', {
+      status: 1,
+      message: 'Successfully sent message',
+      _conversation: _conversation,
+      payload: payload
+    })
+  } catch (e) {
+    io.to(socket.id).emit('send-message-v2', {
+      status: 0,
+      message: 'Failed to send Message'
+    })
+  }
+}
+
+module.exports = {
+  onSendMessage
+}
