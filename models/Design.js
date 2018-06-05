3a734e7bd2dd07e9ffcaccdb1905220ca6f29b77
Jay Lord Torres <isens.jaylord@gmail.com>
2018-06-06 00:36:12 +0800

    index: Design.js

--- models/Design.js
+++ models/Design.js
@@ -8,7 +8,7 @@ const designSchema = new Schema({
   colorThree: { type: String },
   colorFour: { type: String },
   _profilePic: { type: mongoose.Schema.Types.ObjectId, ref: 'MediaUpload' },
-  _host: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
+  _host: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true, index: true  }
 }, {timestamps: true});
 
 module.exports = mongoose.model('Design', designSchema);
