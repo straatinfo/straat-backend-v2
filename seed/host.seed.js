63319de1cd2c56faf4562b58d862b8e48f2b95eb
Jay Lord Torres <isens.jaylord@gmail.com>
2018-06-07 05:40:47 +0800

    seed: host

--- seed/host.seed.js
+++ seed/host.seed.js
@@ -3,9 +3,10 @@ const newUser = new User()
 
 const data = [
   {
-    hostName: 'test Host 3',
-    email: 'testHost3@straat.info',
-    username: 'testHost3',
+    _id: '5a7b485a039e2860cf9dd19a',
+    hostName: 'Gemeente Den Haag',
+    email: 'denhaag@straat.info',
+    username: 'Gemeente Den Haag',
     streetName: 'Postbus 12600',
     city: 'S-GRAVENHAGE',
     country: 'Netherlands',
@@ -13,6 +14,14 @@ const data = [
     phoneNumber: '14070',
     _role: '5a75c9de3a06a627a7e8af45',
     password: newUser.encryptPassword('test')
+  },
+  {
+    _id: '5a844e1bf154bc463543b987',
+    hostName: 'freeHost',
+    email: 'freehost@test.com',
+    username: 'freeHost',
+    postalCode: '2500 DJ',
+    _role: '5a75c9de3a06a627a7e8af45' // should be changed according to _role in db
   }
 ]
 
