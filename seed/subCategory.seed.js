d85baad6402d8b8982f4148054c49fa2bda9a889
Jay Lord Torres <isens.jaylord@gmail.com>
2018-06-07 08:32:16 +0800

    seed: categories

--- seed/subCategory.seed.js
+++ seed/subCategory.seed.js
@@ -3,8 +3,40 @@ const MainCategory = require('../models/MainCategory')
 
 const data = [
   {
-    _mainCategory: '5a788e456f94354ba0856e66',
-    name: 'General'
+    _id: '5af352c7c38e980014415ea5',
+    _mainCategory: '5af3524dc38e980014415e9c',
+    name: 'sub eng 2018 05 09 IIa',
+    description: null
+  },
+  {
+    _id: '5af352d9c38e980014415ea9',
+    _mainCategory: '5af3524dc38e980014415e9c',
+    name: 'sub eng 2018 05 09 IIb',
+    description: null
+  },
+  {
+    _id: '5af352e6c38e980014415ead',
+    _mainCategory: '5af3524dc38e980014415e9c',
+    name: 'other',
+    description: null
+  },
+  {
+    _id: '5af55f19e0936b0014615dd6',
+    _mainCategory: '5af55ee6e0936b0014615dce',
+    name: 'large waste',
+    description: 'large waste'
+  },
+  {
+    _id: '5af55f28e0936b0014615dda',
+    _mainCategory: '5af55ee6e0936b0014615dce',
+    name: 'small chemical waste',
+    description: 'small chemical waste'
+  },
+  {
+    _id: '5af55f01e0936b0014615dd2',
+    _mainCategory: '5af55ee6e0936b0014615dce',
+    name: 'waste next to container',
+    description: 'waste next to container'
   }
 ]
 
