9e5bc43e6fee1e18da4e66cce6d3d193a02c83e4
Jay Lord Torres <isens.jaylord@gmail.com>
2018-06-11 01:01:56 +0800

    feature: socketService Partial

---
+++ service/ServerSocketService.js
@@ -0,0 +1,11 @@
+// const WebSocket = require('ws')
+// const ws = new WebSocket('ws://localhost/')
+// partial
+const socketSend = function () {
+
+
+}
+
+module.exports = function (req, res, next) {
+  // res.socketSend(socketSend)
+}
