aa41f4409ff9e89cac66d3455fc7f8f572bc4c9c
Jay Lord Torres <isens.jaylord@gmail.com>
2018-06-18 20:08:33 +0800

    bugfix: email to host language

--- assets/dictionary/nl.js
+++ assets/dictionary/nl.js
@@ -3,6 +3,7 @@ module.exports = {
   noHostNameForThatAddress: 'Geen hostnaam voor dat adres.',
   noHostAvailableForThisAddress: 'Geen host beschikbaar voor dit adres.',
   noHostAvailableForThisHost: 'Geen host beschikbaar voor deze host',
-  newNotificationOfPublicSpace: 'Nieuwe melding openbare ruimte.',
-  yourNewNotificationPublicSpace: 'Your new notification public space.'
+  newNotificationOfPublicSpace: 'Nieuwe melding openbare ruimte',
+  yourNewNotificationPublicSpace: 'Uw nieuwe melding openbare ruimte'
 }
+
