162409f7ebbd7462ef4d19a7e2a2dbadc8f167cc
John Higgins M. Avila <johnhiggins.avila@gmail.com>
2019-03-29 04:03:46 +0800

    fix

--- handlers/mainCategory/getMaincCategoryWithGeneral.js
+++ handlers/mainCategory/getMaincCategoryWithGeneral.js
@@ -17,7 +17,7 @@ function getMainCategories (req, res, next) {
   const host = req.$scope.host;
 
   return req.db.MainCategory.find({ _host: host._id })
-    .populate('subCategories', ['_id', 'name', 'description'])
+    .populate('subCategories')
     .populate('_reportType', ['_id', 'code', 'name', 'description'])
     .then((mainCategories) => {
       console.log('main categories fetched', mainCategories);
