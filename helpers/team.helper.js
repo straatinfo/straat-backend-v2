b8332d398e1f74a10f9dcedd5726b6251b3e63b1
John <johnhiggins.avila@gmail.com>
2019-12-30 15:23:22 +0800

    update

--- helpers/team.helper.js
+++ helpers/team.helper.js
@@ -70,26 +70,43 @@ const getTeamInfoById = (_id) => {
     }) 
     .populate({
       path: 'teamMembers',
-      populate: {
-        select: {
-          username: true,
-          _id: 1,
-          fname: true,
-          lname: true,
-          houseNumber: true,
-          streetName: true,
-          city: true,
-          postalCode: true,
-          country: true,
-          phoneNumber: true,
-          _profilePic: true,
-          _team: true
+      populate: [
+        {
+          select: {
+            username: true,
+            _id: 1,
+            fname: true,
+            lname: true,
+            houseNumber: true,
+            streetName: true,
+            city: true,
+            postalCode: true,
+            country: true,
+            phoneNumber: true,
+            _profilePic: true,
+            _team: true
+          },
+          path: '_user',
+          populate: {
+            path: '_profilePic'
+          }
         },
-        path: '_user',
-        populate: {
-          path: '_profilePic'
+        {
+          select: {
+            _id: 1,
+            teamName: true,
+            teamEmail: true,
+            _profilePic: true,
+            isVolunteer: true,
+            isApproved: true,
+            _conversation: true
+          },
+          path: '_team',
+          populate: {
+            path: '_profilePic'
+          }
         }
-      }
+      ]
     })
     .populate('_profilePic')
     .populate('_host', [ '_id', 'hostName', 'email' ])
